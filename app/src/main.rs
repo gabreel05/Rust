3899d52f6dd1ac60c154a2b25d1c586573dbfd1d
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2022-09-28 10:22:53 -0300

    Adding subtract function

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,10 +1,14 @@
 fn main() {
     println!("Hello, world!");
     println!("{}", sum(1, 2));
+    println!("{}", subtract(2, 1));
 }
-
 
 // function to get the sum of two numbers
 fn sum(x: i32, y: i32) -> i32 {
     x + y
+}
+
+fn subtract(x: i32, y: i32) -> i32 {
+    x - y
 }
